11138579a0bd19bda85b823119d552df95e642f2
duyanh <duyanh05012004@gmail.com>
2023-01-10 10:54:52 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
+    introDesc: `Mẫn có rảnh hong xem giúp tui này với hehe`,
+    btnIntro: 'OK',
+    title: 'B có thấy bản thân mình cute hongg 😙',
+    desc: 'Trả lời thật lòng nhaa :> ',
+    btnYes: 'Có chứ,rất là cute lun hehe  💖💖💖',
+    btnNo: 'hong nha! 💔 ',
+    question:'Vậy chờ j nữa mà hong gửi những tấm ảnh cute cho tui xem 😊😊',
+    reasonPlaceholder: 'write your message',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Okk để tối tui gửi cho kkk 😊😊',
+    mess: 'ok oke tui sẽ chờ nhé 🥰.Thôi tui đùa tí hah giờ thì nghỉ ngơi đi về với bố mẹ còn sức đi báo nữa 😘😘',
+    messDesc: '💬💬 Byee về tới thì alo tui biết đó 💬💬',
+    btnAccept: ' ---> 💖',
+    messLink: 'https://www.messenger.com/t/100083913669159'
+}
